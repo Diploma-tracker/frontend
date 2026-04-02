81929326cb63a0cac84db45c14b88fdab87c6dd1
Kyrylo Ulianov <kirillulianow@gmail.com>
2026-04-02 17:11:27 +0200

    feat: add some shared assets

--- apps/tracker-dashboard/src/shared/assets/index.ts
+++ apps/tracker-dashboard/src/shared/assets/index.ts
@@ -1,3 +1,4 @@
+import khpiLogo from './images/logo_khpi.webp';
 import univerImg from './images/univer.png';
 
-export { univerImg };
+export { univerImg, khpiLogo };
---
+++ apps/tracker-dashboard/src/shared/components/logo/logo.tsx
@@ -0,0 +1,11 @@
+import { khpiLogo } from '@/shared/assets';
+
+import { Avatar, AvatarImage } from '@repo/ui-kit/components/common/data-display/avatar';
+
+export const Logo = () => {
+  return (
+    <Avatar className="size-8 rounded-lg">
+      <AvatarImage src={khpiLogo} alt="KHPI university logo" />
+    </Avatar>
+  );
+};
