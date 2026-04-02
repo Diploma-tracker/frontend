48d8a005f35fb9f3a243d68c762552c49133a60a
Kyrylo Ulianov <kirillulianow@gmail.com>
2026-04-02 17:12:18 +0200

    chore: added mock user types

--- packages/api-types/src/user/index.ts
+++ packages/api-types/src/user/index.ts
@@ -1,7 +1,15 @@
 export const UserRole = {
-  Admin: 'ADMIN',
-  Staff: 'STAFF',
-  Student: 'STUDENT',
+  Admin: "ADMIN",
+  Staff: "STAFF",
+  Student: "STUDENT",
 } as const;
-
 export type UserRole = (typeof UserRole)[keyof typeof UserRole];
+
+export type User = {
+  id: string;
+  fullName: string;
+  initials: string;
+  email: string;
+  avatarUrl?: string;
+  role: UserRole;
+};
