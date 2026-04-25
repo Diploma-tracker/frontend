2e1fecddcc64a4b2ebc41992b6faab7c32fb31c9
Blackgolyb <andrejomelnickij@gmail.com>
2026-04-25 22:14:48 +0300

    feat: move api setup to the app level

--- apps/tracker-dashboard/src/app/main.tsx
+++ apps/tracker-dashboard/src/app/main.tsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client';
 
 import '@repo/ui-kit/styles.css';
 import './styles/index.css';
-import '../shared/api';
+import './api';
 import { ProvidersWrapper } from './providers';
 
 const rootElement = document.getElementById('root')!;
