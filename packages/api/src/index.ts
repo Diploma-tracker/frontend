4324a55db68815575edf7d088292c553b38a20c2
Blackgolyb <andrejomelnickij@gmail.com>
2026-04-28 22:59:03 +0300

    feat: update api sdk

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -5,3 +5,4 @@ export * as model from "./generated/model";
 export * as auth from "./generated/auth";
 export * as allocationRound from "./generated/allocation-round";
 export * as supervisionApplication from "./generated/supervision-application";
+export * as thesisDefenseSession from "./generated/thesis-defense-session";
