727c4f6bc9c3c81eb60583fdf928c6a9c364e768
Blackgolyb <andrejomelnickij@gmail.com>
2026-04-29 23:16:10 +0300

    feat: add multi select component

--- apps/tracker-dashboard/src/shared/components/index.ts
+++ apps/tracker-dashboard/src/shared/components/index.ts
@@ -3,6 +3,9 @@ export { ConfirmationModal } from './confirmation-modal/confirmation-modal';
 export { TextFormField } from './form/text-form-field';
 export { PasswordFormField } from './form/password-form-field';
 export { DatePickerFormField } from './form/date-picker-form-field';
+export { DurationPickerFormField } from './form/duration-picker-form-field';
+export { MultiSelect } from './form/multi-select';
+export type { MultiSelectOption } from './form/multi-select';
 
 export { Container } from './layout/container/container';
 
