d32d80d20c1ab4dfb2513d443036b8703d9d391e
Blackgolyb <andrejomelnickij@gmail.com>
2026-05-02 01:25:23 +0300

    chore: clean up

--- apps/tracker-dashboard/src/pages/schedule/index.tsx
+++ apps/tracker-dashboard/src/pages/schedule/index.tsx
@@ -4,12 +4,9 @@ import { StudentDefenseSessions } from '@/modules/defense';
 import '@/shared/components/week-calendar/schedule.css';
 
 export const SchedulePage = function SchedulePage() {
-  // <div className="h-[500px] overflow-y-hidden border-2 border-red-500 p-10">
-  // <div className="h-screen max-h-screen overflow-y-hidden border-2 border-red-500 p-10">
   return (
     <PageLayout height="screen">
       <StudentDefenseSessions />
-      {/*</div>*/}
     </PageLayout>
   );
 };
