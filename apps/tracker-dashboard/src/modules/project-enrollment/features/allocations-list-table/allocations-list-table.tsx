91d165cea86b2eb0f216bd7671ff6debb152ad47
Kyrylo Ulianov <kirillulianow@gmail.com>
2026-05-05 19:22:40 +0200

    feat: set permission for actions in allocations table only for admin

--- apps/tracker-dashboard/src/modules/project-enrollment/features/allocations-list-table/allocations-list-table.tsx
+++ apps/tracker-dashboard/src/modules/project-enrollment/features/allocations-list-table/allocations-list-table.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 
+import { userAtom } from '@/modules/user';
 import {
   DataTable,
   type TableDataConfig,
@@ -7,13 +8,17 @@ import {
 import { reatomComponent } from '@reatom/react';
 
 import { type AllocationRoundDTO, allocationRoundListAtom } from '../../models';
-import { columns } from './columns';
+import { createAllocationRoundColumns } from './columns';
 
 export const AllocationsListTable = reatomComponent(
   function AllocationsListTable() {
+    const user = userAtom();
+
     const status = allocationRoundListAtom.status();
     const allocationRounds = allocationRoundListAtom.data();
     const filter = allocationRoundListAtom.filter();
+
+    const columns = createAllocationRoundColumns(user);
 
     const tableDataConfig: TableDataConfig<AllocationRoundDTO> = {
       data: allocationRounds?.items ?? [],
--- apps/tracker-dashboard/src/modules/project-enrollment/features/allocations-list-table/columns/index.ts
+++ apps/tracker-dashboard/src/modules/project-enrollment/features/allocations-list-table/columns/index.ts
@@ -1,3 +1,5 @@
+import { permissions } from '@/modules/auth';
+import type { User } from '@/modules/user';
 import { type ColumnDef } from '@tanstack/react-table';
 
 import type { AllocationRoundDTO } from '../../../models';
@@ -7,10 +9,19 @@ import { NameColumn } from './name-column';
 import { StartDateColumn } from './start-date-column';
 import { StatusColumn } from './status-column';
 
-export const columns: ColumnDef<AllocationRoundDTO>[] = [
-  NameColumn,
-  StartDateColumn,
-  EndDateColumn,
-  StatusColumn,
-  ActionColumn,
-];
+export const createAllocationRoundColumns = (
+  user: User,
+): ColumnDef<AllocationRoundDTO>[] => {
+  const baseColumns = [
+    NameColumn,
+    StartDateColumn,
+    EndDateColumn,
+    StatusColumn,
+  ];
+
+  if (permissions.isAdmin(user)) {
+    return [...baseColumns, ActionColumn];
+  }
+
+  return baseColumns;
+};
