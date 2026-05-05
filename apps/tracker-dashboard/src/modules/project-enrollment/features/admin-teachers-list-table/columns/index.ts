f05049b6a4bff891452f77f1693a31ffd5a0a646
Kyrylo Ulianov <kirillulianow@gmail.com>
2026-05-05 18:30:07 +0200

    chore: added todo

---
+++ apps/tracker-dashboard/src/modules/project-enrollment/features/admin-teachers-list-table/columns/index.ts
@@ -0,0 +1,21 @@
+import { getRowSelectionColumn } from '@/shared/components';
+import { type ColumnDef } from '@tanstack/react-table';
+
+import type { TeacherDTO } from '../../../models';
+import { createTeacherActionColumn } from './action-column';
+import { TeacherApplicationsColumn } from './applications-column';
+import { TeacherEmailColumn } from './email-column';
+import { TeacherNameColumn } from './name-column';
+import { TeacherSelectionColumn } from './selection-column';
+
+//TODO: hide selection column when allocation round is not in "draft" status
+export const createTeacherColumns = (
+  roundId: string,
+): ColumnDef<TeacherDTO>[] => [
+  getRowSelectionColumn(),
+  TeacherNameColumn,
+  TeacherEmailColumn,
+  TeacherSelectionColumn,
+  TeacherApplicationsColumn,
+  createTeacherActionColumn(roundId),
+];
