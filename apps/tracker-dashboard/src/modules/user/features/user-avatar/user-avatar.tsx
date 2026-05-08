8c15b0b3a22cd3b0a30e5b6db593705d78920ecc
Kyrylo Ulianov <kirillulianow@gmail.com>
2026-05-08 12:36:44 +0200

    chore: left todo

--- apps/tracker-dashboard/src/modules/user/features/user-avatar/user-avatar.tsx
+++ apps/tracker-dashboard/src/modules/user/features/user-avatar/user-avatar.tsx
@@ -7,16 +7,20 @@ import {
   AvatarFallback,
   AvatarImage,
 } from '@repo/ui-kit/components/common/data-display/avatar';
+import { cn } from '@repo/ui-kit/lib/utils';
 
 import { userAtom } from '../../models';
 import { userQuery } from '../../models/users-model';
 
 type UserAvatarProps = {
   userId: string;
+  className?: string;
 };
 
+// TODO: for consistency we should create stable size variants for avatar instead hardcoded values
 export const UserAvatar = reatomComponent(function UserAvatar({
   userId,
+  className,
 }: UserAvatarProps) {
   const { data } = useQuery(userQuery, userId);
 
@@ -27,7 +31,7 @@ export const UserAvatar = reatomComponent(function UserAvatar({
   const initials = getCapitalsFromStrings(firstName, lastName);
 
   return (
-    <Avatar className="size-8">
+    <Avatar className={cn('size-8', className)}>
       <AvatarImage
         src=""
         alt={`${isCurrentUser ? 'your' : 'user'} avatar image`}
